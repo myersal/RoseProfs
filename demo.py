50858c1fe55a3dc5f5ec772b514b3444cfd577eb
myersal <myersal@rose-hulman.edu>
2017-04-18 22:04:57 -0400

    edited it to be correctly commented

--- demo.py
+++ demo.py
@@ -1,19 +1,19 @@
-//create connection 
+#create connection 
 client = pyorient.OrientDB("localhost", 2424) 
 session_id = client.connect( "admin", "admin" )
  
-//create a databse 
+#create a databse 
 client.db_create( db_name, pyorient.DB_TYPE_GRAPH, pyorient.STORAGE_TYPE_MEMORY ) 
 
-//open databse 
+#open databse 
 client.db_open( DB_Demo, "admin", "admin" ) 
 
-//create class 
+#create class 
 cluster_id = client.command( "create class my_class extends V" ) 
 
-//create property 
+#create property 
 cluster_id = client.command( "create property my_class.id Integer" ) 
 cluster_id = client.command( "create property my_class.name String" ) 
 
-//insert record 
+#insert record 
 client.command("insert into my_class ( 'id','’name' ) values( 1201, 'satish')") 
