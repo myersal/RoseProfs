6b2cbe5b320b40c10f176ad0405bfc824dc73d5e
myersal <myersal@rose-hulman.edu>
2017-04-21 00:27:22 -0400

    fixed naming error

--- demo.py
+++ demo.py
@@ -10,7 +10,7 @@ client.db_open("roseprofs", "admin", "admin" );
 
 #find proffessor and student, then create edge
 
-proffessors = client.command("select * from prof where name = " + "'Goebel'");
+professors = client.command("select * from prof where name = " + "'Goebel'");
 students = client.command("select * from stud where username = " + "'suckup'");
 
 currentEdges = client.command("select * from prof_rate where out = " + students[0]._rid + " and in = " + professors[0]._rid);
@@ -18,7 +18,7 @@ currentEdges = client.command("select * from prof_rate where out = " + students[
 if(len(currentEdges) == 0):
 	#insert edge
 
-	new_edge = client.command("create edge prof_rate from " + students[0]._rid + " to " + proffessors[0]._rid + " set cool = " + "1" + ", help = " + "2" + ", comm = " + "3" + ", grad = " + "4");
+	new_edge = client.command("create edge prof_rate from " + students[0]._rid + " to " + professors[0]._rid + " set cool = " + "1" + ", help = " + "2" + ", comm = " + "3" + ", grad = " + "4");
 
 	print(client.command("select * from prof_rate"));
 	
