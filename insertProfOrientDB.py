c58c4dfb56f4d8df77de15b2d5bafe28d53542ca
myersal <myersal@rose-hulman.edu>
2017-04-21 00:50:18 -0400

    adding insert students

---
+++ insertProfOrientDB.py
@@ -0,0 +1,23 @@
+#insert a new student into the database and check for errors
+import pyorient
+import pyorient.ogm
+
+#create connection
+client = pyorient.OrientDB("localhost", 2424);
+session_id = client.connect( "root", "wai3feex" );
+
+#open databse 
+client.db_open("roseprofs", "admin", "admin" );
+
+#find proffessor and student, then create edge
+
+username = raw_input("insert a student to add to the database: ");
+
+professors = client.command("select * from prof where name = '%s'" % username);
+
+if(len(professors) != 0):
+		new_edge = client.command("create vertex stud set name = %s" % username);
+		print(client.command("select * from prof_rate"));
+		
+	else:
+		print("the user has already rated the professor");
