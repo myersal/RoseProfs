5b9c34c1e711c1ae2e258fc6cc61ddf4ae1dedeb
myersal <myersal@rose-hulman.edu>
2017-04-21 00:23:47 -0400

    added an error check

--- demo.py
+++ demo.py
@@ -13,10 +13,14 @@ client.db_open("roseprofs", "admin", "admin" );
 proffessors = client.command("select * from prof where name = " + "'Goebel'");
 students = client.command("select * from stud where username = " + "'suckup'");
 
-print(students[0]);
+currentEdges = client.command("select * from prof_rate where out = " + proffessors[0]._rid + " and in = " + students[0]._rid);
 
-#insert edge
+if(len(currentEdges) == 0):
+	#insert edge
 
-new_edge = client.command("create edge prof_rate from " + students[0]._rid + " to " + proffessors[0]._rid + " set cool = " + "1" + ", help = " + "2" + ", comm = " + "3" + ", grad = " + "4");
+	new_edge = client.command("create edge prof_rate from " + students[0]._rid + " to " + proffessors[0]._rid + " set cool = " + "1" + ", help = " + "2" + ", comm = " + "3" + ", grad = " + "4");
 
-print(client.command("select * from prof_rate")); 
+	print(client.command("select * from prof_rate"));
+	
+else:
+	print("the user has already rated the professor");
