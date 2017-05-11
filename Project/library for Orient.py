3bea46ca599a0bb6e4a2ba4f9828a29ed5f29a67
myersal <myersal@rose-hulman.edu>
2017-05-11 19:25:48 -0400

    added auth of

--- Project/library for Orient.py
+++ Project/library for Orient.py
@@ -137,7 +137,7 @@ def sortByAuthor():
 
 def sortByISBN():
 		print('all books sorted by isbn')
-		result = client.command("select EXPAND(IN()) from book ORDER BY isbn")
+		result = client.command("select EXPAND(both('auth_of')) from book ORDER BY isbn")
 		
 		for data in result:
 			print(data.oRecordData)
