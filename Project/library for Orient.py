e7a4cc58c3fa3c9df44fe5b9daadd199c3840a60
myersal <myersal@rose-hulman.edu>
2017-05-11 21:40:45 -0400

    tried goebels

--- Project/library for Orient.py
+++ Project/library for Orient.py
@@ -144,10 +144,15 @@ def sortByISBN():
 		#result = client.command("Select expand( from (TRAVERSE both('auth_of') FROM (Select * from book) WHILE $depth <= 1)) ORDER BY isbn")
 		#result = client.command("SELECT expand($c) Let $a = (select * from book), $b = (select expand(in()) book), $c = unionall($a, $b)")
 		#result = client.command("SELECT $c, title, isbn, pages from book LET $c = (Select expand(in()) from book) where $c.isbn = isbn")
-		result = client.command("SELECT in(), title, isbn, pages from book")
+		result = client.command("SELECT in() AS author, title, isbn, pages from book")
 		for data in result:
 			print('1');
-			print(data)
+			print("isbn: " + data['isbn']),
+			print(" title: "data['title']),
+			print(" pages: "data['pages']),
+			for d in data['author']:
+				print(" name: " + d['name'])
+			
 
 def sortByPages():
 		print('all books sorted by number of pages')
